dde44a6c7886d6bc38e805ecfd864d5173d7adb2
Tom Nelson <tkoinaustin@yahoo.com>
2016-12-31 08:21:09 -0600

    Added nice intro segue

---
+++ Slider/Segues/IntroSegue.swift
@@ -0,0 +1,31 @@
+//
+//  IntroSegue.swift
+//  Slider
+//
+//  Created by Tom Nelson on 12/31/16.
+//  Copyright © 2016 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+
+class IntroSegue: UIStoryboardSegue {
+  
+  override func perform() {
+    let tiny = CGAffineTransform.identity.scaledBy(x: 0, y: 0)
+    destination.view.transform = tiny
+    source.view.addSubview(destination.view)
+    
+    UIView.transition(
+      with: UIApplication.shared.keyWindow!,
+      duration: 0.75,
+      options: UIViewAnimationOptions(),
+      animations: {
+        self.destination.view.transform = CGAffineTransform.identity
+    },
+      completion: { _ in
+        self.destination.view.removeFromSuperview()
+        UIApplication.shared.keyWindow?.rootViewController =
+          self.destination
+    })
+  }
+}
